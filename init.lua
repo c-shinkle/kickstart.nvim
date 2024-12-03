ecf92a0f26f2a8ead3b348f69ca7ea5ad78b32e0
Christian Shinkle <cshinkle2018@gmail.com>
2024-12-03 07:06:09 -0600

    added typescript LSP

--- init.lua
+++ init.lua
@@ -452,6 +452,11 @@ require('lazy').setup({
   },
   { 'Bilal2453/luvit-meta', lazy = true },
   {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
